7b8b2efe1ea8660633216be23b893d6998f7991c
Kirill <104451159+Modestra@users.noreply.github.com>
2022-05-10 00:11:57 +0700

    Update OutputPayLine.cs

--- OutputPayLine.cs
+++ OutputPayLine.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class OutputPayLine : MonoBehaviour
 {
+    private Transform point;
+    public GameObject goal;
     void Start()
     {
         
@@ -12,7 +14,11 @@ public class OutputPayLine : MonoBehaviour
     void Update()
     {
         RaycastHit hit;
-        Ray ray = new Ray(transform.position, transform.up);
-        Debug.DrawLine(transform.position, transform.up, Color.red);
+        Ray ray = new Ray(transform.position, -transform.up*1000);
+        Debug.DrawLine(transform.position, -transform.up*1000, Color.red);
+        if (Physics.Raycast(ray, out hit))
+        {
+            goal = Instantiate(goal, new Vector3(0,0,0), Quaternion.identity);
+        }
     }
 }
